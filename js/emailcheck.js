104fa0af840f52b757f14b37cf9febd0911ebf0a
Rahul Shah <rahuldshah3@gmail.com>
2018-09-11 03:45:44 -0400

    check msg as well

--- js/emailcheck.js
+++ js/emailcheck.js
@@ -1,16 +1,22 @@
 function check()
 {
-  if(!document.getElementById("email").checkValidity())
+  if(!document.getElementById("email").checkValidity() && isValidMsg(document.forms['frm'].message.value))
   {
     alert("input not valid!");
   } 
   else
   {
-    callMeIfValid();
+    isValid();
   }
 }
 
-function callMeIfValid()
+function isValid()
 {
     M.toast({html: 'Message Sent!', classes: 'rounded'});
 }
+
+function isValidMsg(str)
+{
+    return str.replace(/^\s+/g, '').length;
+}
+
